933c59c9428d77d86814ca467dbd7fe58d46f333
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2025-05-17 16:40:41 -0500

    onceavo commit

---
+++ TALLERGRUPALCHATBOX/Models/GeminiResponse.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TALLERGRUPALCHATBOX.Models
+{
+    public class GeminiResponse
+    {
+        [JsonPropertyName("candidates")]
+        public List<Candidate> Candidates { get; set; }
+    }
+
+    public class Candidate
+    {
+        [JsonPropertyName("content")]
+        public ContentResponse Content { get; set; }
+    }
+
+    public class ContentResponse
+    {
+        [JsonPropertyName("parts")]
+        public List<Part> Parts { get; set; }
+    }
+
+}
