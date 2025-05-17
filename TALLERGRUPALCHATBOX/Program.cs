f9fc681a01729af7caec29aee992a44a2b07bbdd
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2025-05-17 15:59:28 -0500

    noveno commit

--- TALLERGRUPALCHATBOX/Program.cs
+++ TALLERGRUPALCHATBOX/Program.cs
@@ -1,7 +1,13 @@
+using TALLERGRUPALCHATBOX.Interfaces;
+using TALLERGRUPALCHATBOX.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<IChatBoxService, GeminiRepository>();
+
 
 var app = builder.Build();
 
